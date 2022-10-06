6822984180edcd68c63f6cf546d3f88253953240
AviyaCG <aviyacg@gmail.com>
2022-10-06 21:54:26 +0300

    Add square click functionality and gmaeplay module

--- game.js
+++ game.js
@@ -1,15 +1,23 @@
 
 const BOARD = (function(){
-    let squares =   ['X', '', '',
-                     'X', '', '',
-                     '', 'O', ''
-                    ];
+    let squares =   [
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ];
     const squareElements = document.querySelectorAll('.square');
     const renderSquares = () => {
         squares.forEach((mark, index) => squareElements[index].innerHTML = mark);
     };
 
-    return{renderSquares};
+    const move = (mark, index) => {
+        if(squares[index] != '') return;
+        squares[index] = mark;
+        squareElements[index].innerHTML = mark;
+        renderSquares();
+    };
+
+    return{renderSquares, move};
 })();
 
 const playerFactory = (name, mark) => {
@@ -19,4 +27,16 @@ const playerFactory = (name, mark) => {
 // create two players
 const player1 = playerFactory('player 1', 'X');
 const player2 = playerFactory('player 2', 'O');
+
+const GAMEPLAY = (function(){
+    let currentPlayer = player1;
+
+    const squareElements = document.querySelectorAll('.square');
+    const clickSquare = (e) => {
+        const index = e.target.dataset.index;
+        BOARD.move(currentPlayer.mark, index);// make move 
+        currentPlayer = currentPlayer === player1 ? player2 : player1;// switch player
+    };
+    squareElements.forEach(square => square.addEventListener('click', clickSquare));
+})(player1, player2);
 
